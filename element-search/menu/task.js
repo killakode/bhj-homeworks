0999f7896b627b4facd5e45a239f4d60f18fe83a
killakode <sudarev.n@yandex.ru>
2023-08-05 15:10:27 +0300

    меню

---
+++ element-search/menu/task.js
@@ -0,0 +1,16 @@
+let links = Array.from(document.querySelectorAll('.menu__link'));
+
+for (let link of links) {
+	link.onclick = function () {
+		let parent = link.parentElement;
+		if (parent.querySelector('.menu_sub').className == 'menu menu_sub') {
+			parent.querySelector('.menu_sub').className = 'menu menu_sub menu_active';
+		} else {
+			parent.querySelector('.menu_sub').className = 'menu menu_sub';
+		}
+
+		if (link.closest('.menu_main')) {
+			return false;
+		}
+	}
+}
