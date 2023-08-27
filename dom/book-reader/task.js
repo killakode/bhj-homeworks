206ccbe20986b7e59463fa401a02fe0fe1917118
killakode <sudarev.n@yandex.ru>
2023-08-28 01:19:49 +0300

    reader finale commit

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,43 +1,27 @@
-let book = document.getElementsByClassName("book")[0];
-let settings = book.getElementsByClassName("book__controls")[0].children;
+let book = document.querySelector('.book');
 
-[...settings].forEach(setting => {
-	let settingName = setting.classList[1];
-	let options = setting.querySelectorAll("a.font-size");
-	let optionsName = options[0].classList[0];
 
-	let getActiveIndex = () => {
-		let index = 0;
-		while (!options[index].classList.contains(optionsName + "_active")) index++;
-		return index;
-	};
+let fonstSizes = [...document.querySelectorAll('.font-size')];
 
-	let click = e => {
-		e.preventDefault();
-		let activeIndex = getActiveIndex();
-		options[activeIndex].classList.remove(optionsName + "_active");
-		e.target.classList.add(optionsName + "_active");
-		activeIndex = getActiveIndex();
-
-		switch (settingName) {
-			case "book__control_font-size":
-				book.classList.remove("book_fs-big");
-				book.classList.remove("book_fs-small");
-				switch (activeIndex) {
-					case 0:
-						book.classList.add("book_fs-small");
-						break;
-					case 2:
-						book.classList.add("book_fs-big");
-						break;
-				}
-				break;
-
-			
-		}
-	};
-
-  [...options].forEach(option => {
-		option.addEventListener("click", click, false);
-	});
-});
+let changeFontSize = function () {
+    fonstSizes.forEach(elem => {
+        elem.addEventListener('click', (event) => {
+            event.preventDefault();
+            fonstSizes.forEach(elem => {
+                elem.classList.remove('font-size_active');
+            });
+            elem.classList.add('font-size_active');
+            if(elem.classList.contains('font-size_small')) {
+                book.classList.add('book_fs-small');
+                book.classList.remove('book_fs-big');
+            } else if(elem.classList.contains('font-size_big')) {
+                book.classList.add('book_fs-big');
+                book.classList.remove('book_fs-small');
+            } else {
+                book.classList.remove('book_fs-small');
+                book.classList.remove('book_fs-big');
+            }
+        });
+    });
+}
+changeFontSize();
