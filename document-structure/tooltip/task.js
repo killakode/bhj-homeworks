139a20bc57d6c6bd503e5a23f19180dde3f1a6d7
killakode <sudarev.n@yandex.ru>
2023-09-11 19:19:38 +0300

    tooltip

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,25 +1,33 @@
-const toolTips = [...document.querySelectorAll('.has-tooltip')];
+let toolTips = [...document.querySelectorAll('.has-tooltip')];
 
 toolTips.forEach(toolTip => {
   toolTip.addEventListener('click', (e) => {
     e.preventDefault();
 
     // Добавление контейнера для подсказки
-    const toolTipContainer = document.createElement('div');
+    let toolTipContainer = document.createElement('div');
     toolTipContainer.classList.add('tooltip_container');
 
     // Добавление подсказки
-    const toolTipContent = document.createElement('div');
+    let toolTipContent = document.createElement('div');
     toolTipContent.classList.add('tooltip');
     toolTipContent.textContent = toolTip.getAttribute('title');
     toolTipContent.classList.add('tooltip_active');
 
     // Закрытие подсказки при повторном клике на ссылку
-    const toolTipActive = toolTip.querySelector('.tooltip_active');
+    let toolTipActive = toolTip.querySelector('.tooltip_active');
     if(toolTipActive) {
         toolTipActive.remove();
         return;
     }
+
+    let coordinates = toolTip.getBoundingClientRect();
+    let coordinatesTop = coordinates.top + window.pageYOffset;
+    let coordinatesLeft = coordinates.left + window.pageXOffset;
+
+    toolTipContent.style.top = coordinatesTop + coordinates.height + 'px';
+    toolTipContent.style.left = coordinatesLeft + 'px';
+
 
     toolTip.appendChild(toolTipContent);
   });
