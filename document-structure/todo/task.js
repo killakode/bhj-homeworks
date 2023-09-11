84de129d118e0b0bb0f6cc3c498afa2ef8b97634
killakode <sudarev.n@yandex.ru>
2023-09-11 19:19:15 +0300

    todo

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -3,22 +3,6 @@ let addTask = document.querySelector('.tasks__add');
 let taskList = document.querySelector('.tasks__list');
 
 
-
-inputTask.addEventListener('keydown', function(event) {
-    if(event.key === 'Enter' && inputTask.value.trim() !== '') {
-        taskList.innerHTML += `
-            <div class="task">
-                <div class="task__title">
-                    ${inputTask.value}
-                </div>
-                <a href="#" class="task__remove">&times;</a>
-            </div>
-        `;
-        inputTask.value = '';
-        addTaskToLocalStorage();
-    }
-});
-
 addTask.addEventListener('click', function(event) {
     event.preventDefault();
     if(inputTask.value.trim() === '') {
