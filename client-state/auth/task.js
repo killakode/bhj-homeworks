7caa521009de2a06e6ea2010200b2bcb04be8e26
killakode <sudarev.n@yandex.ru>
2023-09-16 20:05:25 +0300

    auth

---
+++ client-state/auth/task.js
@@ -0,0 +1,39 @@
+const form = document.getElementById('signin__form');
+const welcome = document.getElementById('welcome');
+const signin = document.getElementById('signin');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+        if(xhr.response.success) {
+            const userId = xhr.response.user_id;
+            localStorage.setItem('userId', userId);
+            logoutBtn.classList.add('logout__btn_active');
+            welcomeDude(userId);
+        } else {
+            alert('Неверный логин или пароль');
+        }
+    }        
+    xhr.send(formData);
+    form.reset();
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    const userId = localStorage.getItem('userId');
+    if(userId) {
+        welcomeDude(userId);
+    }
+});
+
+
+// Функция приветствия пользователя 
+function welcomeDude(userId) {
+    signin.classList.remove('signin_active');
+    welcome.classList.add('welcome_active');
+    welcome.querySelector('#user_id').textContent = userId;
+}
